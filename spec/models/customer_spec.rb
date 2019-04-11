2a9f53f69eea3aa27f40a0b671fa4546b8fe01df
ttam <mattgooglin@gmail.com>
2019-04-10 20:59:07 -0400

    Adds specs(tests) for required fields of customer model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -18,17 +18,58 @@ RSpec.describe Customer, type: :model do
     lead = Customer.new(
         pGUID: nil
     )
-
     lead.valid?
     expect(lead.errors[:pGUID]).to include("can't be blank")
 
   end
-  it 'is invalid without a pAccName'
-  it 'is invalid without a pPartner'
-  it 'is invalid without a name'
-  it 'is invalid without a business_name'
-  it 'is invalid without a tel_number'
-  it 'is invalid without an email'
+
+  it 'is invalid without a pAccName' do
+    lead = Customer.new(
+                       pAccName: nil
+    )
+    lead.valid?
+    expect(lead.errors[:pAccName]).to include("can't be blank")
+  end
+
+  it 'is invalid without a pPartner' do
+    lead = Customer.new(
+                       pPartner: nil
+    )
+    lead.valid?
+    expect(lead.errors[:pPartner]).to include("can't be blank")
+  end
+
+  it 'is invalid without a name' do
+    lead = Customer.new(
+                       name: nil
+    )
+    lead.valid?
+    expect(lead.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a business_name' do
+    lead = Customer.new(
+                       business_name: nil
+    )
+    lead.valid?
+    expect(lead.errors[:business_name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a tel_number' do
+    lead = Customer.new(
+                       tel_number: nil
+    )
+    lead.valid?
+    expect(lead.errors[:tel_number]).to include("can't be blank")
+  end
+
+  it 'is invalid without an email' do
+    lead = Customer.new(
+                       email: nil
+    )
+    lead.valid?
+    expect(lead.errors[:email]).to include("can't be blank")
+  end
 
   it 'is invalid with a name of more than 100 chars'
   it 'is invalid with a business_name of more than 100 chars'
