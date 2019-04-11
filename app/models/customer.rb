d284b2f8ee79c90907b444d480f1d1b79aededbf
ttam <mattgooglin@gmail.com>
2019-04-11 09:49:08 -0400

    Adds simple email format validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,9 @@
 class Customer < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :pGUID, :pAccName, :pPartner, :name, :business_name,
             :tel_number, :email, presence: true
   validates :name, :business_name, length: { maximum: 100 }
   validates :tel_number, length: { maximum: 13 }
-  validates :email, length: { maximum: 80 }
+  validates :email, length: { maximum: 80 },
+            format: { with: VALID_EMAIL_REGEX }
 end
