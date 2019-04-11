f9d8d077285d75d1f8690216c2e64d624a5606d7
ttam <mattgooglin@gmail.com>
2019-04-11 15:06:37 -0400

    Adds helper to display errors with customer and adds create action to customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,35 @@
-class CustomerController < ApplicationController
+class CustomersController < ApplicationController
 
   def new
     @customer = Customer.new
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    @customer.pGUID = SecureRandom.uuid
+    if @customer.valid?
+      # submit to api
 
+      resp = HTTParty.post(ENV['LEAD_API_URI'], {
+          body: "access_token=#{ENV['LEAD_API_ACCESS_TOKEN']}&pGUID=#{@customer.pGUID}&pAccName=#{@customer.pAccName}&pPartner=#{@customer.pPartner}",
+          headers: {
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'charset' => 'utf-8'
+          }
+      })
+      byebug
+    else
+      render 'new'
+    end
+    # byebug
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:pAccName, :pPartner, :name,
+                                      :business_name, :tel_number,
+                                      :email, :contact_time,
+                                      :notes, :reference)
   end
 end
--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,2 @@
-module CustomerHelper
+module CustomersHelper
 end
